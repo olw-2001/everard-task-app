021a669c6f0a1737af3635c37c805bfe27c32daa
Oliver Warrilow <oliw2014@outlook.com>
2023-01-06 17:34:20 +0000

    First commit

---
+++ database/migrations/2023_01_06_164426_create_parts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('parts', function (Blueprint $table) {
+            $table->id();
+            $table->string('part_number', 100);
+            $table->string('part_description', 200);
+            $table->integer('stock_quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('parts');
+    }
+};
